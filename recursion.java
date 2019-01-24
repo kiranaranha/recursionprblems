43bb1fba0e3ec16ad3ee48c3bda21d05ed40003a
kiranaranha <kiranaranha@students.berkeley.net>
2019-01-24 12:56:22 -0800

    -

--- recursion.java
+++ recursion.java
@@ -15,9 +15,21 @@ public int fibonacci(int n){
 }
 
 public int countHi(String str) {
-        int count = 0;
-        for (int i = 0; i < str.length()-1; i++)
+    int count = 0;
+    for (int i = 0; i < str.length()-1; i++){}
         if (str.substring(i, i+2).equals("hi"))
         count += 1;
         return count;
-        }
+}
+
+public int countHi(String str) {
+    if (str.length() < 2){
+        return 0;
+    }
+    if (str.substring(0,2).equals("hi")){
+        return 1 + countHi(str.substring(1));
+    }else{
+        return countHi(str.substring(1));
+    }
+}
+
